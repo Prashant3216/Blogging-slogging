35781f014ba3d81bcd076e5c1d503900c6506f19
Prashant3216 <pv3216@gmail.com>
2022-10-27 19:08:55 +0530

    sechema defined

---
+++ src/backend/model/user.model.js
@@ -0,0 +1,17 @@
+const {Schema, model}=require("mongoose")
+
+const userSchema=new Schema({
+
+    firstName: {type: Stirng, required: true},
+    lastName: String,
+    age: {type: Number, min:13, max:100},
+    email:{type: String},
+    gender: {type: String, enum:["male" || "female" || "other" || "opt out"]},
+    password: {type:String},
+    role: {type: String, num:["admin" || "reader"]}
+})
+
+
+const userModel = model(user, userSchema)
+
+module.exports=userModel
