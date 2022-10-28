9917f8cec6a96babbe39452e0f520efbd43fe33a
Prashant3216 <pv3216@gmail.com>
2022-10-28 17:39:54 +0530

    seperated user route

--- src/backend/server.js
+++ src/backend/server.js
@@ -1,60 +1,17 @@
 const express=require('express')
-const { body, validationResult } = require('express-validator');
 const  mongoose = require('mongoose')
-const jwt = require("jsonwebtoken")
-
-const userModel=require('./model/user.model')
-
+const userRoute=require("./model/user.route")
 
 const app=express()
 
 app.use(express.urlencoded({extended:true}))
 
 app.use(express.json())
+app.use("/user", userRoute)
 
 app.get('/', (req, res)=>{res.send('home')})
 
 
-app.post('/signup',
-body("email").isEmail(),
-body("password").isLength({min:8}),
- async(req, res)=>{
-    const error =validationResult(req)
-    if(!error.isEmpty())
-    return res.status(400).send({ errors: error });
-    const userdata=req.body
-    const userfound= await userModel.findOne({email:userdata.email})
-    if (userfound){
-        return res.status(409).send("your are already registed with us, please proceed to login")
-    }
-    const user=new userModel({...userdata, role:userdata.email==="pv3216@gmail.com"? "admin": "reader"})
-    await user.save();
-    const mainToken=jwt.sign({email:user.email, role:user.role}, "SECRETKEY009", {
-        expiresIn:"10 seconds"
-    })
-    const refreshToken=jwt.sign({email:user.email, role:user.role}, "SECRETKEY0099", {
-        expiresIn:"10 minutes"
-    })
-    res.send({mainToken:mainToken, refreshToken:refreshToken})
-    return res.status(200).send("your account is created successfully")
-})
-
-app.post("/login", async (req,res)=>{
-    const userData=req.body
-    const matchData= await userModel.findOne({email:userData.email, password:userData.password})
-    if (!matchData){
-        return res.status(401).send("Invalid email or password")
-    }
-    const mainToken=jwt.sign({email:matchData.email, role:matchData.role}, "SECRETKEY009", {
-        expiresIn:"10 seconds"
-    })
-    const refreshToken=jwt.sign({email:matchData.email, role:matchData.role}, "SECRETKEY0099", {
-        expiresIn:"10 minutes"
-    })
-    res.send({mainToken:mainToken, refreshToken:refreshToken})
-    res.send("Sucessfully logged in")
-    
-})
 
 app.listen(8080, async()=>{
     await mongoose.connect("mongodb://127.0.0.1:27017/blogslog")
